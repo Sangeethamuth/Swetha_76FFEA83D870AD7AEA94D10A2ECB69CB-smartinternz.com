c96d191144807d3d85c76949fdbca041555114ff
Sangeethamuth <144891753+Sangeethamuth@users.noreply.github.com>
2023-10-13 13:12:40 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,12 +1,10 @@
-def isLeapyear(year):
-  if (year % 4 ==0 and year % 100!= 0) or year % 400 == 0:
-   return True
+# Python Program to find the Factorial of a number using Recursion
+#Fact_Recur.py
+def fact(m):
+  if m<=1:
+     return 1
   else:
-   return False
-
-year = int(input("Enter a year :"))
-
-if isLeapyear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'. format(year))
+     return m*fact(m-1)
+# Main Program
+n=int(input("Enter the value of n:"))
+print("The factorial of",n,"is", fact(n))
